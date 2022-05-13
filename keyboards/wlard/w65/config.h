2e7eb74fd6a7732893976adcf9f5d5c46b62c0da
Jan Lunge <janlunge1993@gmail.com>
2022-05-13 04:36:03 +0200

    config for 65% on qmk works

--- keyboards/wlard/w65/config.h
+++ keyboards/wlard/w65/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "config_common.h"
+// #include "config_common.h"
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xEEEE
@@ -13,9 +13,10 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 16
 
+
 #define MATRIX_ROW_PINS { D1, C0, B0, B1, B3 }
 #define MATRIX_COL_PINS { B16, B17, D0, A1, A2, D7, B2, D4, D2, D3, C3, C4, C6, C7, E30, C5 }
-#define UNUSED_PINS
+// #define UNUSED_PINS { C1 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -25,3 +26,11 @@
 #define VIAL_KEYBOARD_UID {0x0A, 0x2B, 0xCE, 0xEC, 0xFA, 0x91, 0x40, 0x3F}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// i2c_master defines
+// #define I2C1_SCL_PIN B0 // A2 on pinout = B0
+// #define I2C1_SDA_PIN B1 // A3 on pinout = B1
+// #define I2C1_SCL_PAL_MODE PAL_MODE_ALTERNATIVE_2
+// #define I2C1_SDA_PAL_MODE PAL_MODE_ALTERNATIVE_2
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
--- keyboards/wlard/w65/rules.mk
+++ keyboards/wlard/w65/rules.mk
@@ -7,3 +7,22 @@ BOOTLOADER = halfkay
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+DEFAULT_FOLDER = wlard/w65
+
+LAYOUTS = ortho_1x1
