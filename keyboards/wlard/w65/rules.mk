f5e8266a300e66150c0f53c39d418e114debc6aa
Jan Lunge <janlunge1993@gmail.com>
2022-05-13 03:47:38 +0200

    added chibios config files

--- keyboards/wlard/w65/rules.mk
+++ keyboards/wlard/w65/rules.mk
@@ -1,13 +1,9 @@
 # MCU name
-MCU = MKL26Z64  #cortex-m0plus
+MCU = MKL26Z64
 USE_CHIBIOS_CONTRIB = yes
 
+# Bootloader selection
 BOOTLOADER = halfkay
 
-EXTRAKEY_ENABLE 	= yes
-NKRO_ENABLE 		= yes
-CONSOLE_ENABLE		= yes
-COMMAND_ENABLE		= yes
-MOUSEKEY_ENABLE 	= yes
-
+# Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
